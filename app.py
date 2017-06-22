d358d3df56f892ecbc19d858baf296d0eebaa09c
p0lish <polish1987@gmail.com>
2017-06-22 20:05:56 +0200

    add processes

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from multiprocessing import Process
+
+import webservice
 import yotsuba_bot
 
-
 if __name__ == '__main__':
-    yotsuba_bot.main()
+    yotsuba = Process(target=yotsuba_bot.main)
+    webservice = Process(target=webservice.main)
+
+    yotsuba.start()
+    webservice.start()
+
 
---
+++ webservice.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    res = random.uniform(1, 10)
+    return str(res)
+
+def main():
+    app.run(port=8080)
